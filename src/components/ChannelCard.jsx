9c633014451c5f81aaa6de69625286685b6fcc57
bsextion <bsextion@gmail.com>
2025-02-11 19:54:00 -0600

    added video feed feature, api call to get suggested videos

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,11 @@
+import { Box, Card, CardContent } from '@mui/material'
+import React from 'react'
+
+
+const ChannelCard = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ChannelCard
