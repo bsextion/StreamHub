b616b51df5d280605222bd045163a7a0bc6afd2b
bsextion <bsextion@gmail.com>
2025-02-14 19:51:34 -0600

    added channel detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Videos, ChannelCard } from "./index";
+import { fetchFromAPI } from "../utils/services/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
 
-export default ChannelDetail
+  console.log(videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, #542740, #4f2246, #471e4e, #391e56, #1f1f5f)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+        <Box display="flex" p="2">
+          <Box sx={{mr: {sm: "100px"}}}/>
+            <Videos videos={videos} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
